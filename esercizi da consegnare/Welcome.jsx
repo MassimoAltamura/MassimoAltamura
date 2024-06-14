0224ef2f13eaf5cf5fec7bcd4c3d1e584fedd061
MassimoAltamura <158032882+MassimoAltamura@users.noreply.github.com>
2024-06-14 16:24:37 +0200

    Add files via upload

---
+++ esercizi da consegnare/Welcome.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+export function Welcome() {
+  return (
+      <div className="welcome">
+          <h1>Welcome to my React App</h1>
+      </div>
+  );
+}
+
+export default Welcome;
