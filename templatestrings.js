540e54f3fb3c96629db36c14e80a0403afce200b
Massimo Altamura <158032882+MassimoAltamura@users.noreply.github.com>
2024-09-12 10:21:00 +0200

    Add files via upload

---
+++ templatestrings.js
@@ -0,0 +1,5 @@
+const multiLineString = `Questa è una stringa su più righe.
+Puoi scrivere su una nuova riga senza usare caratteri speciali.
+Le template string rendono questo molto semplice!`;
+
+console.log(multiLineString);
